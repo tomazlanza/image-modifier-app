f1fa379e30976fff03b431b8a009b85bf3a82eb3
Tomaz Lanza <lanza.tomaz@gmail.com>
2024-03-12 23:42:46 -0300

    implementing single endpoint input

--- serverController.mjs
+++ serverController.mjs
@@ -9,7 +9,7 @@ import ejs from 'ejs'
 const app = express()
 const port = 8080
 
-let originalImage
+let base64InputImage
 
 let inputIndicator = false
 
@@ -17,19 +17,7 @@ let inputIndicator = false
 
 app.engine(".html", ejs.__express)
 app.use(express.static('public'))
-app.use(bodyParser.raw({
-  type: 'image/gif',
-  limit: '5mb'
-}))
-app.use(bodyParser.raw({
-  type: 'image/png',
-  limit: '5mb'
-}))
-app.use(bodyParser.raw({
-  type: 'image/jpeg',
-  limit: '5mb'
-}))
-app.use(express.json())
+app.use(express.json({ limit: '20mb' }))
 app.use(corsEnabler)
 app.set("views", "./views")
 // setting routes
@@ -41,40 +29,63 @@ app.get("/", (req, res) => {
 })
 
 // app.post("/image", async (req, res) => {
+// app.post("/image", (req, res) => {
+
+//   // if(req.body) {
+//   //   //do a validation
+//   // }
+//   originalImage = req.body
+//   res.end("Image has been succesfully received!")
+// })
+
+// app.post("/rgb-values-input", (req, res) => {
 app.post("/image", (req, res) => {
-
-  // if(req.body) {
-  //   //do a validation
-  // }
-  originalImage = req.body
-  res.end("Image has been succesfully received!")
-})
-
-app.post("/rgb-values-input", (req, res) => {
 
   // if(req.body){
   //   do a validation 
   // }
 
-  const rgbvalues = req.body
+  const requestBody = req.body
+  
+  const rgbvalues = requestBody.rgbValues
+  base64InputImage = requestBody.imageUpload
+
+  let mediaType = base64InputImage.split(',')[0].split(';')[0].split(':')[1]
+  console.log(mediaType)
+  const base64ImageString = base64InputImage.split(',')[1] 
+  
+  const binaryImageData = atob(base64ImageString)
+
+  const byteArray = new Uint8Array(binaryImageData.length)
+
+  for (let i = 0; i < binaryImageData.length; i++) {
+    byteArray[i] = binaryImageData.charCodeAt(i)
+  }
+
+  // Create a Blob from the binary data
+  const blob = new Blob([byteArray], { type: mediaType }) // Adjust the MIME type as needed
+
+  // Create a File object from the Blob
+  const buffer = Buffer.from(binaryImageData, 'binary');
+
 
   async function sendingResultToClient() {
     try {
-      const filteredImage = await imageFilter(originalImage, rgbvalues)
+      const filteredImage = await imageFilter(buffer, rgbvalues)
     
-      const originalImageBase64 = originalImage.toString('base64')
+      // const originalImageBase64 = originalImage.toString('base64')
       const filteredImageBase64 = filteredImage.toString('base64')
       
       inputIndicator = true
       
       res.render("main.html", {
         inputIndicator: inputIndicator,
-        originalImage: originalImageBase64,
+        originalImage: base64ImageString,
         filteredImage: filteredImageBase64,
       })
     } catch (error) {
       console.error("Error processing image:", error)
-      res.status(500).send("Error processing image. Please, try again.")
+      res.status(500).send(`Error processing image: ${error}. Please, try again.`)
     }
   }
 
